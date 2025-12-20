887d7e3664fd283da859f42234eb4e88dd4f6c53
GoGradually <chkun3109@gmail.com>
2025-12-20 16:55:20 +0900

    feat: 초기 Spring Boot 프로젝트 설정 및 파일 추가

---
+++ src/main/java/me/pinitgateway/PinitGatewayApplication.java
@@ -0,0 +1,13 @@
+package me.pinitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinitGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinitGatewayApplication.class, args);
+    }
+
+}
